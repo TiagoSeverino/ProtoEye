7e4ecb24d105b8d365d9baebc0cbebebdefdba22
TiagoSeverino <tiago@severino.xyz>
2018-05-03 20:32:31 +0100

    Organize File Location

--- camera.py
+++ camera.py
@@ -3,8 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-H = 'C:\Users\Bruno\Desktop\OpenCV_RescueMaze\H_grayscale.png'
-S = 'C:\Users\Bruno\Desktop\OpenCV_RescueMaze\S_grayscale.png'
+H = 'char/H.png'
+S = 'char/S.png'
 
 cap = cv2.VideoCapture(1) #0 is the default camera of the laptop, 1 is the USB camera
 template = cv2.imread(S, cv2.IMREAD_GRAYSCALE)  
@@ -15,27 +15,27 @@ cv2.imshow('template', template)
 cv2.waitKey(25)
 
 while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+	# Capture frame-by-frame
+	ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Our operations on the frame come here
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	# Display the resulting frame
+	cv2.imshow('frame',gray)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
-    w, h = template.shape[::-1]
-    res = cv2.matchTemplate(gray,template,  1)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+	w, h = template.shape[::-1]
+	res = cv2.matchTemplate(gray,template,  1)
+	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-    top_left = min_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv2.rectangle(frame,top_left, bottom_right, 255, 2)
-    
-    cv2.imshow('frame', frame)
-    cv2.waitKey(25)
+	top_left = min_loc
+	bottom_right = (top_left[0] + w, top_left[1] + h)
+	cv2.rectangle(frame,top_left, bottom_right, 255, 2)
+	
+	cv2.imshow('frame', frame)
+	cv2.waitKey(25)
 
 
 
